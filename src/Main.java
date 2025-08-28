31192a21eb26e78e5c553885fd8645750e049482
Emilio Leon <zS23017360@estudiantes.uv.mx>
2025-08-28 15:28:09 -0600

    algorithm to take and show an user input 3

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Ingresa tu nombre:");
         String name = input.nextLine();
-
-        for (int i = 0; i < 3; i++) {
             System.out.println("Hola!, " + name);
-        }
     }
 }
