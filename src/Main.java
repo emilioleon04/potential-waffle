efd9dd690f9c8dc629c54aed281fa434cf40e7e5
Emilio Leon <zS23017360@estudiantes.uv.mx>
2025-08-28 15:35:12 -0600

    algorithm to take and show an user input new features

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,16 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Ingresa tu nombre:");
         String name = input.nextLine();
+
             System.out.println("Hola!, " + name);
+
+            Scanner user = new Scanner(System.in);
+        System.out.println("Ingresa tu edad:");
+        int edad = user.nextInt();
+
+        if  (edad >= 18) {
+            System.out.printf("Felicidades!, " + name + " puedes votar en la elecciones!");
+        }
 
     }
 }
